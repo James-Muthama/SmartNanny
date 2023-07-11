2c3ac8c7bcc4286bd5c74b7cb2897f33f4de7293
James-Muthama <jamesmuthaiks@gmail.com>
2023-07-12 02:53:42 +0300

    Moved class_nanny.py, class_nanny_collection.py,Added def recommending_days and def checking_if_nanny_free_on_the_days to class_nanny_collection.py

--- SmartNanny_backend/nanny_backend/class_nanny_collection.py
+++ SmartNanny_backend/nanny_backend/class_nanny_collection.py
@@ -43,7 +43,58 @@ class NannyCollection:
 
         self.collection.update_one({"_id": _id}, salary_change)
 
-    def connecting_nanny_to_customer(self, dates, _id):
+    def checking_if_nanny_free_on_the_days(self, dates):
         for date in dates:
-            date = date[date]
+            # Checking if a date is equal to null
+            availability = self.collection.find_one({date: {'$ne': "null"}}).count()
+
+            return availability
+
+    def recommending_days(self, dates):
+        date = []
+        query = {
+            '$or': [
+                {'Mon': "null"},
+                {'Tue': "null"},
+                {'Wed': "null"},
+                {'Thur': "null"},
+                {'Fri': "null"},
+                {'Sat': "null"}
+            ]
+        }
+        for _ in dates:
+            results = self.collection.find_one(query)
+
+            # Iterate over the results and extract the specific value
+            matching_value = None
+
+            for document in results:
+                if 'Mon' in document:
+                    matching_value = document['Mon']
+                    break
+                elif 'Tue' in document:
+                    matching_value = document['Tue']
+                    break
+                elif 'Wed' in document:
+                    matching_value = document['Wed']
+                    break
+                elif 'Thur' in document:
+                    matching_value = document['Thur']
+                    break
+                elif 'Fri' in document:
+                    matching_value = document['Fri']
+                    break
+                elif 'Sat' in document:
+                    matching_value = document['Sat']
+                    break
+
+            print(matching_value)
+
+
+
+
+
+
+
+
 
