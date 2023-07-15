8c811b8cb9cbb1ccc2282d5a27bd644a85bcae3c
James-Muthama <jamesmuthaiks@gmail.com>
2023-07-16 02:48:02 +0300

    Modified connecting_nanny_to_employee.py

--- SmartNanny_backend/nanny_backend/method/connecting_nanny_to_employee.py
+++ SmartNanny_backend/nanny_backend/method/connecting_nanny_to_employee.py
@@ -1,7 +1,9 @@
 from SmartNanny_backend.nanny_backend.method.getting_nanny_details import dates, nanny_id
 from SmartNanny_backend.nanny_backend.classes.class_nanny_collection import NannyCollection
 from SmartNanny_backend.customer_backend.methods.insert_customer import customer_id
+from SmartNanny_backend.nanny_backend.method.checking_if_nanny_free_on_the_days import available_nannies
 
 nanny_collection = NannyCollection("SmartNanny", "Nanny")
 
-nanny_collection.connecting_nanny_to_employee(dates, customer_id, nanny_id)
+if available_nannies > 0:
+    nanny_collection.connecting_nanny_to_employee(dates, customer_id, nanny_id)
