6c75660ad725caf036f82f48df87660c73c64647
James-Muthama <jamesmuthaiks@gmail.com>
2023-07-22 18:52:27 +0300

    Added converting_input_to_numpy_array.py

---
+++ Smart_Nanny/SmartNanny_chatbot/converting_input_to_numpy_array.py
@@ -0,0 +1,26 @@
+import nltk
+
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer = LancasterStemmer()
+
+import numpy as np
+
+
+# create a function that takes in input from a user converts it into a numpy array to feed into the model
+def bag_of_words(s, words):
+    # creates the bag of words
+    bag = [0 for _ in range(len(words))]
+
+    # tokenizes every word inputted by the user and stems them
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    # fills the bag of words with 1 if a stemmed word matches a word from the words list
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+
+    # returns a numpy array to be passed in the model
+    return np.array(bag)
