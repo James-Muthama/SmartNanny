5eb2867d74dfbabde04909bc6e3dcda31a7332f3
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-01 10:40:52 +0300

    Modified connecting_to_chrome_driver.py

--- Smart_Nanny/Whatsapp_API/connecting_to_chrome_driver.py
+++ Smart_Nanny/Whatsapp_API/connecting_to_chrome_driver.py
@@ -8,9 +8,7 @@ print(driver.title)
 
 
 def read_message():
-    unread_text = driver.find_element("l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl riy2oczp dsh4tgtl "
-                                      "sy6s5v3r gz7w46tb lyutrhe2 qfejxiq4 fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt "
-                                      "j90th5db aumms1qt")
+    unread_text = driver.find_element("")
 
 
 time.sleep(10)
