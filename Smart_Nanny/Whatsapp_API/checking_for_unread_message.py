0256248b4e8649df80fc066bd21ddbb4a35cc4f3
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-03 01:03:50 +0300

    Modified checking_for_unread_message.py.py and checking_for_unread_message.py Added main.py

--- Smart_Nanny/Whatsapp_API/checking_for_unread_message.py
+++ Smart_Nanny/Whatsapp_API/checking_for_unread_message.py
@@ -1,11 +1,11 @@
-from connecting_to_chrome_driver import driver
+from selenium.webdriver.common.by import By
 
 
-def finding_unread_message():
+def finding_unread_message(driver):
     try:
-        unread_message = driver.find_element(
-            "l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl riy2oczp dsh4tgtl sy6s5v3r gz7w46tb "
-            "lyutrhe2 qfejxiq4 fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt j90th5db aumms1qt")
+        unread_message = driver.find_element(By.CLASS_NAME, "l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl "
+                                                            "riy2oczp dsh4tgtl sy6s5v3r gz7w46tb lyutrhe2 qfejxiq4 "
+                                                            "fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt j90th5db aumms1qt")
 
         unread_message = unread_message.text
 
--- Smart_Nanny/Whatsapp_API/opening_whatsapp.py
+++ Smart_Nanny/Whatsapp_API/opening_whatsapp.py
@@ -1,4 +1,11 @@
 from connecting_to_chrome_driver import driver
+from checking_for_unread_message import finding_unread_message
+import time
 
-whatsapp_page = driver.get("https://web.whatsapp.com")
+driver.get("https://web.whatsapp.com")
+
+time.sleep(10)
+
+unread_message = finding_unread_message(driver)
+
 
