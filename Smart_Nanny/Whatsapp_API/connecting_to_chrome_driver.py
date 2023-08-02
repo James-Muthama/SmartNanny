65b900e5dc098e808e85504396101e837a499ec6
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-02 18:35:53 +0300

    Modified connecting_to_chrome_driver.py Added opening_whatsapp.py

--- Smart_Nanny/Whatsapp_API/connecting_to_chrome_driver.py
+++ Smart_Nanny/Whatsapp_API/connecting_to_chrome_driver.py
@@ -1,9 +1,3 @@
 from selenium import webdriver
-import time
 
 driver = webdriver.Chrome()
-
-driver.get("https://web.whatsapp.com")
-print(driver.title)
-
-time.sleep(10)
---
+++ Smart_Nanny/Whatsapp_API/opening_whatsapp.py
@@ -0,0 +1,4 @@
+from connecting_to_chrome_driver import driver
+import time
+
+whatsapp_page = driver.get("https://web.whatsapp.com")
