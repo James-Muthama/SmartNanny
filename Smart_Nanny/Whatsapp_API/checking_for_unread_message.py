731aa97808f373d81cfbb290e626ced98ac52cc0
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-02 22:19:58 +0300

    Added checking_for_unread_message.py.py

---
+++ Smart_Nanny/Whatsapp_API/checking_for_unread_message.py
@@ -0,0 +1,15 @@
+from connecting_to_chrome_driver import driver
+
+
+def finding_unread_message():
+    try:
+        unread_message = driver.find_element(
+            "l7jjieqr cfzgl7ar ei5e7seu h0viaqh7 tpmajp1w c0uhu3dl riy2oczp dsh4tgtl sy6s5v3r gz7w46tb "
+            "lyutrhe2 qfejxiq4 fewfhwl7 ovhn1urg ap18qm3b ikwl5qvt j90th5db aumms1qt")
+
+        unread_message = unread_message.text
+
+        return unread_message
+
+    except:
+        driver.close()
