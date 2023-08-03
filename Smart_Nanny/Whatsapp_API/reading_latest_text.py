2888b619ce65156327df2048b2e9b0277785c681
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-03 20:50:04 +0300

    Added reading_latest_text.py

---
+++ Smart_Nanny/Whatsapp_API/reading_latest_text.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def reading_latest_text(driver):
+    try:
+        all_texts = WebDriverWait(driver, 15).until(
+            EC.presence_of_all_elements_located((By.CLASS_NAME, "_11JPr selectable-text copyable-text"))
+        )
+        if all_texts:
+            latest_text = all_texts[-1]
+
+            return latest_text
+    except:
+        print("Cannot find texts")
