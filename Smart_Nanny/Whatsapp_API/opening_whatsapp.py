695dd2c632e868c16515a12feff7735093590daa
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-03 19:05:25 +0300

    Modified opening_whatsapp.py

--- Smart_Nanny/Whatsapp_API/opening_whatsapp.py
+++ Smart_Nanny/Whatsapp_API/opening_whatsapp.py
@@ -1,5 +1,6 @@
 from connecting_to_chrome_driver import driver
 from checking_for_unread_message import finding_unread_message
+from opening_unread_message import opening_unread_chat
 import time
 
 driver.get("https://web.whatsapp.com")
@@ -7,5 +8,7 @@ driver.get("https://web.whatsapp.com")
 time.sleep(10)
 
 unread_message = finding_unread_message(driver)
+
+opening_unread_chat(unread_message, driver)
 
 
