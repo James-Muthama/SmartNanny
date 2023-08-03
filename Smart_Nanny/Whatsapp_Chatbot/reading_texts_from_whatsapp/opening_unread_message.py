e17b7b92fdd07de3536f6248b4f6fd2a28cee6d3
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-03 21:02:56 +0300

    Renamed package Whatsapp_Api to Whatsapp_Chatbot

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/opening_unread_message.py
+++ Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/opening_unread_message.py
@@ -1,4 +1,4 @@
-from Smart_Nanny.Whatsapp_API.reading_texts_from_whatsapp.finding_unread_chat import finding_unread_chat
+from Smart_Nanny.Whatsapp_Chatbot.reading_texts_from_whatsapp.finding_unread_chat import finding_unread_chat
 
 
 def opening_unread_chat(unread_message, driver):
