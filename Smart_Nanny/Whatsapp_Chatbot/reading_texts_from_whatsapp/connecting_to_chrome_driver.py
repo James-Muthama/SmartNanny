422928aeca67d88352b71098713dbd3234f8d08f
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-08 00:08:47 +0300

    Modified connecting_to_chrome_driver.py

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/connecting_to_chrome_driver.py
+++ Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/connecting_to_chrome_driver.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
 
 options = webdriver.ChromeOptions()
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("--user-data-dir=C:/Users/mywhatsapp")
+options.add_argument("user-data-dir = C:/Users/James Muthama/AppData/Local/Google/Chrome/User Data")
 driver = webdriver.Chrome(options=options)
 
 
