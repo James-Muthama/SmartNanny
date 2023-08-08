2cf2f55ead24d68478437c3a17f202da31c20d21
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-08 03:08:28 +0300

    Modified connecting_to_chrome_driver.py

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/connecting_to_chrome_driver.py
+++ Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/connecting_to_chrome_driver.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
 
-options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir = C:/Users/James Muthama/AppData/Local/Google/Chrome/User Data")
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome()
 
 
