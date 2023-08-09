d0f1444c7c934eee82e98a7d26496498ab8a4202
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-09 18:27:32 +0300

    Deleted opening_unread_message.py, reading_latest_text.py and reading_text_from_whatsapp.py

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/opening_unread_message.py
+++
@@ -1,8 +0,0 @@
-from Smart_Nanny.Whatsapp_Chatbot.reading_texts_from_whatsapp.finding_unread_chat import finding_unread_chat
-
-
-def opening_unread_chat(unread_message, driver):
-    unread_chat = finding_unread_chat(unread_message, driver)
-
-    unread_chat.click()
-
