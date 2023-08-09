03ef0638425cb73c3a16d23aa5423314c63db5b6
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-09 18:26:34 +0300

    Modified connecting_to_chrome_driver.py

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/connecting_to_chrome_driver.py
+++ Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/connecting_to_chrome_driver.py
@@ -2,6 +2,6 @@ from selenium import webdriver
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome("chromedriver.exe", chrome_options=options)
 
 
