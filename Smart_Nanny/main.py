da9b01ddc4c11789979ffbc1a8c0838a3147ba09
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-15 15:25:02 +0300

    Modified main.py

--- Smart_Nanny/main.py
+++ Smart_Nanny/main.py
@@ -1,5 +1,8 @@
 from Smart_Nanny.SmartNanny_chatbot.chat import chat
+from Smart_Nanny.Whatsapp_Chatbot.opening_whatsapp import opening_whatsapp
 from Smart_Nanny.Whatsapp_Chatbot.reading_texts_from_whatsapp.reading_customer_text import reading_customer_text
+
+opening_whatsapp()
 
 text = reading_customer_text()
 
