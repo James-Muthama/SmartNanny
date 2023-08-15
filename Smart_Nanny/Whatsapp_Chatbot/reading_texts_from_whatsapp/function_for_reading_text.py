58d4a8bfc2ecf54d036aaf121a46abcc6d749533
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-15 15:21:38 +0300

    Modified function_for_reading_text.py

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/function_for_reading_text.py
+++ Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/function_for_reading_text.py
@@ -1,12 +1,5 @@
-from Smart_Nanny.Whatsapp_Chatbot.reading_texts_from_whatsapp.connecting_to_chrome_driver import driver
+from Smart_Nanny.Whatsapp_Chatbot.connecting_to_chrome_driver import driver
 import time
-
-
-def opening_whatsapp():
-    # opening whatsapp
-    driver.maximize_window()
-    driver.get("https://web.whatsapp.com/")
-    time.sleep(120)
 
 
 def checking_for_unread_message():
