124534da3134eee8a32a122c6d954a7529341256
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-15 15:21:56 +0300

    Modified reading_customer_text.py

--- Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/reading_customer_text.py
+++ Smart_Nanny/Whatsapp_Chatbot/reading_texts_from_whatsapp/reading_customer_text.py
@@ -2,13 +2,10 @@ from function_for_reading_text import *
 
 
 def reading_customer_text():
-    opening_whatsapp()
+    unread_message = checking_for_unread_message()
 
-    while True:
-        unread_message = checking_for_unread_message()
+    opening_unread_chat(unread_message)
 
-        opening_unread_chat(unread_message)
+    text = reading_chat(unread_message)
 
-        text = reading_chat(unread_message)
-
-        return text
+    return text
