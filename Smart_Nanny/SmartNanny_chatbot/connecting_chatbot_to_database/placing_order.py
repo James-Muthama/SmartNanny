0491355e5f347a45c7ddf6ff1d4a02b629cedbdc
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-18 03:13:13 +0300

    Modified placing_order.py

--- Smart_Nanny/SmartNanny_chatbot/connecting_chatbot_to_database/placing_order.py
+++ Smart_Nanny/SmartNanny_chatbot/connecting_chatbot_to_database/placing_order.py
@@ -24,14 +24,39 @@ def checking_nanny_availability(sentence):
     else:
         dates = nanny_collection.recommending_days(days)
 
-        number_of_days_recommended = len(dates)
+        if isinstance(dates, list):
+            siku = []
+            number_of_days_recommended = len(dates)
 
-        if number_of_days_recommended == 1:
-            return "Unfortunately we don't have househelps available on that day. But we have a househelp available " \
-                   "on {day}.".format(day=dates)
-        elif number_of_days_recommended == 2:
-            return "Unfortunately we don't have househelps available on that day. But we have a househelp available " \
-                   "on {day1} and {day2}.".format(day1=dates[0], day2=dates[1])
-        elif number_of_days_recommended == 3:
-            return "Unfortunately we don't have househelps available on that day. But we have a househelp available " \
-                   "on {day1}, {day2} and {day3}.".format(day1=dates[0], day2=dates[1], day3=dates[2])
+            abbrev_to_full = {
+                "Mon": "Monday",
+                "Tue": "Tuesday",
+                "Wed": "Wednesday",
+                "Thur": "Thursday",
+                "Fri": "Friday",
+                "Sat": "Saturday",
+            }
+
+            for date in dates:
+                siku.append(abbrev_to_full.get(date))
+
+            if number_of_days_recommended == 1:
+                return "Unfortunately we don't have househelps available on that day. But we have a househelp " \
+                       "available on {day}.".format(day=siku)
+            elif number_of_days_recommended == 2:
+                return "Unfortunately we don't have househelps available on that day. But we have a househelp " \
+                       "available on {day1} and {day2}.".format(day1=siku[0], day2=siku[1])
+            elif number_of_days_recommended == 3:
+                return "Unfortunately we don't have househelps available on that day. But we have a househelp " \
+                       "available on {day1}, {day2} and {day3}.".format(day1=siku[0], day2=siku[1], day3=siku[2])
+            elif number_of_days_recommended == 4:
+                return "Unfortunately we don't have househelps available on that day. But we have a househelp " \
+                       "available on {day1}, {day2}, {day3} and {day4}.".format(day1=siku[0], day2=siku[1],
+                                                                                day3=siku[2], day4=siku[3])
+            elif number_of_days_recommended == 5:
+                return "Unfortunately we don't have househelps available on that day. But we have a househelp " \
+                       "available on {day1}, {day2}, {day3}, {day4} and {day5}.".format(day1=siku[0], day2=siku[1],
+                                                                                        day3=siku[2], day4=siku[3],
+                                                                                        day5=siku[4])
+            else:
+                return dates
