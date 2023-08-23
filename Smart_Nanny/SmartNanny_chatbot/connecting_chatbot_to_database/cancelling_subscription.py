5f4854817243b25f332eed9c4514ded4904e7ebf
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-23 21:23:03 +0300

    Modified cancelling_subscription.py

--- Smart_Nanny/SmartNanny_chatbot/connecting_chatbot_to_database/cancelling_subscription.py
+++ Smart_Nanny/SmartNanny_chatbot/connecting_chatbot_to_database/cancelling_subscription.py
@@ -5,7 +5,16 @@ nanny_collection = NannyCollection("SmartNanny", "Nanny")
 customer_collection = CustomerCollection("SmartNanny", "Customer")
 
 
-def cancelling_subscription(name):
+def cancelling_subscription(text):
+    # Split the text by spaces
+    words = text.split()
+
+    # Find the index of "is" in the list of words
+    index_of_is = words.index("is")
+
+    # Extract the name by joining the words after "is"
+    name = " ".join(words[index_of_is + 1:])
+
     customer_id, days = customer_collection.delete_customer(name)
     nanny_collection.deleting_customer_from_nanny_collection(customer_id, days)
 
