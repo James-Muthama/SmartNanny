61caceb2ed7c640e3570885324554bf54b89e0d7
James-Muthama <jamesmuthaiks@gmail.com>
2023-08-23 21:16:43 +0300

    Modified cancelling_subscription.py

--- Smart_Nanny/SmartNanny_chatbot/connecting_chatbot_to_database/cancelling_subscription.py
+++ Smart_Nanny/SmartNanny_chatbot/connecting_chatbot_to_database/cancelling_subscription.py
@@ -3,6 +3,8 @@ from Smart_Nanny.SmartNanny_backend.nanny_backend.classes.class_nanny_collection
 
 nanny_collection = NannyCollection("SmartNanny", "Nanny")
 customer_collection = CustomerCollection("SmartNanny", "Customer")
+
+
 def cancelling_subscription(name):
     customer_id, days = customer_collection.delete_customer(name)
     nanny_collection.deleting_customer_from_nanny_collection(customer_id, days)
